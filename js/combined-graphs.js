9d206386576c7ebfde9550cbb48306831f0d58e7
msc2913 <msc2913@g.coppellisd.com>
2018-05-24 18:08:50 -0500

    Create combined-graphs.js

---
+++ js/combined-graphs.js
@@ -0,0 +1,43 @@
+google.charts.load('current', {packages: ['corechart', 'line']});
+google.charts.setOnLoadCallback(drawBackgroundColor);
+
+function drawBackgroundColor() {
+  var data = new google.visualization.DataTable();
+  data.addColumn('number', 'X');
+  data.addColumn('number', 'Enterprise Rentals');
+  data.addColumn('number', 'Enterprise Rentals 2');
+  data.addColumn('number', 'Alamo Rentals');
+  data.addColumn('number', 'Bronco Rentals');
+  
+  data.addRows([
+    [0, 0, 150, 55, 100],
+    [1, 60, 210, 100, 150],
+    [2, 120, 270, 145, 200],
+    [3, 180, 330, 190, 250],
+    [4, 240, 390, 235, 300],
+    [5, 300, 450, 280, 350],
+    [6, 360, 510, 325, 400],
+    [7, 420, 570, 370, 450],
+    [8, 480, 630, 415, 500],
+    [9, 540, 690, 560, 550],
+    [10, 600, 750, 505, 600]
+  ]);
+  
+  var options = {
+    hAxis: {
+      title: 'Days'
+    },
+    vAxis: {
+      title: 'Cost'
+    },
+    title: {
+      'Enterprise Rentals'
+    },
+    backgroundColor: '#ffffff'
+    width: 900,
+    height: 500
+  };
+  
+  var chart = new google.visualization.LineChart(document.getElementById('graph1'));
+  chart.draw(data, options);
+}
